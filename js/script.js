9c7ca3266a57dcd8d5bdb65b43bd525b558ea256
laura_z <llzerr@hotmail.com>
2022-12-31 12:21:08 -0700

    Update js file to reveal random img

--- js/script.js
+++ js/script.js
@@ -6,6 +6,26 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const res = await fetch ("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    // console.log(images);
+    selectRandomImage(images);
 };
-getImage();
+
+const selectRandomImage = function (images) {
+  let randomIndex = Math.floor(Math.random() * images.length);
+  let randomImage = images[randomIndex];
+  displayImage(randomImage);
+  // console.log(randomImage);
+  // console.log(randomIndex);
+};
+
+const displayImage = function (randomImage) {
+  let author = randomImage.author;
+  let imageAddress = randomImage.download_url;
+  authorSpan.innerText = author;
+  img.src = imageAddress;
+  imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+  getImage();
+});
